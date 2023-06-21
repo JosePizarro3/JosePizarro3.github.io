1b88df760a7b4ff63fc0f49b403c9832876c9a2c
Jose Pizarro <jose.pizarro@physik.hu-berlin.de>
2023-06-21 15:56:47 +0200

    Fixed script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-const currentLocation = location.href;
-const menuItem = document.querySelectorAll(".menu a");
+    const currentLocation = location.href;
+    const menuItem = document.querySelectorAll(".menu a");
 
-menuItem.forEach(function(item) {
-    if (currentLocation === item.href) {
-    item.classList.add("active");
-    }
-});
+    menuItem.forEach(function(item) {
+        if (currentLocation === item.href) {
+            item.classList.add("active");
+        }
+    });
 });
