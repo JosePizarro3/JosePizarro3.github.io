e463c0a1b8a9639eab2de5e1dea7a071a441e2ad
Jose Pizarro <jose.pizarro@physik.hu-berlin.de>
2023-06-22 15:21:44 +0200

    Added footer

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ document.addEventListener("DOMContentLoaded", function() {
 function downloadResume() {
     window.location.href = 'components/CV_JoseMPizarro.pdf'
 }
+
+window.addEventListener("scroll", function() {
+    const menuHeader = document.querySelector(".menu-header");
+    const scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 0) {
+      menuHeader.classList.add("scrolled");
+    } else {
+      menuHeader.classList.remove("scrolled");
+    }
+  });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTop = document.getElementById("scrollToTop");
+
+    scrollToTop.addEventListener("click", function() {
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        });
+    });
+});
