d877bcdf99f1bdf5182cd8ee23f50bdffdee0649
Jose Pizarro <jose.pizarro@physik.hu-berlin.de>
2023-06-22 14:39:35 +0200

    Fixed active

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const currentPathname = location.pathname;
-    const menuItem = document.querySelectorAll(".menu a");
-
+    const menuItem = document.querySelectorAll(".menu ul li a");
+  
     menuItem.forEach(function(item) {
-        if (currentPathname === item.pathname) {
-            item.classList.add("active");
-        }
+      if (currentPathname === item.pathname || currentPathname === `${item.pathname}` || (currentPathname === "" && item.pathname === "/index.html")) {
+        item.classList.add("active");
+      }
     });
-});
+  });
 
 function downloadResume() {
     window.location.href = 'components/CV_JoseMPizarro.pdf'
